7504db3b909cd091d82225a46fa129341e7870db
DorohovaM <105015634+DorohovaM@users.noreply.github.com>
2023-12-27 19:01:51 +0300

    a

---
+++ tasks/task_1/dorohova_m_min_in_matrix_rows/mins_in_matrix_rows.h
@@ -0,0 +1,16 @@
+// Copyright 2023 Dorokhova Marya
+#ifndef MODULES_TASK_1_DOROHOVA_M_MINS_IN_MATRIX_ROWS_MINS_IN_MATRIX_ROWS_H_
+#define MODULES_TASK_1_DOROHOVA_M_MINS_IN_MATRIX_ROWS_MINS_IN_MATRIX_ROWS_H_
+
+#include <vector>
+#include <string>
+
+std::vector<int> getRandomMatrix(int m = 5, int n = 5);
+int getSequentialMinInVec(const std::vector<int>& vector);
+std::vector<int> getSequentialMinsInMatrix(const std::vector<int>& matr, int m, int n);
+std::vector<int> getParallelMinsInMatrix(const std::vector<int>& matr, int m, int n);
+
+void printVector(const std::vector<int>& vec);
+void printMatrix(const std::vector<int>& matr, int m, int n);
+
+#endif  // MODULES_TASK_1_DOROHOVA_M_MINS_IN_MATRIX_ROWS_MINS_IN_MATRIX_ROWS_H_
