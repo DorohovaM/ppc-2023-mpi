4f12cb404364f1c58a02c6f77ff0ef3edc600cb1
DorohovaM <105015634+DorohovaM@users.noreply.github.com>
2023-12-27 19:59:59 +0300

    ф

---
+++ tasks/task_2/dorokhova_m_sleeping_barber/sleeping_barber.h
@@ -0,0 +1,10 @@
+// Copyright 2023 Dorokhova Marya
+#ifndef TASKS_TASK_2_DOROKHOVA_M_SLEEPING_BARBER_SLEEPING_BARBER_H_
+#define TASKS_TASK_2_DOROKHOVA_M_SLEEPING_BARBER_SLEEPING_BARBER_H_
+
+void CutTime(double visitorTime);
+void Barber(double visitorTime, int size);
+void Visitor(double visitorTime, int rank);
+void SleepingBarber();
+
+#endif  // TASKS_TASK_2_DOROKHOVA_M_SLEEPING_BARBER_SLEEPING_BARBER_H_
